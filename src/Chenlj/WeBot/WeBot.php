b13a1fba717e945ee2a1da0ac20128e2bd6e4a47
chenlianjiang <770687650@qq.com>
2017-04-03 21:37:11 +0000

    优化

--- src/Chenlj/WeBot/WeBot.php
+++ src/Chenlj/WeBot/WeBot.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace Chenlj\WeBot;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
 use symfony\var_dumper;
 
 class WeBot {
     public function test($value='')
     {
         echo __METHOD__;
-	dump('chenlj')
+	    dump('chenlj');
     }
 }
+// $test
